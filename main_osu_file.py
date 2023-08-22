9af586de15bc53753a81dfef4148c37f686a4940
maor <68875758+maor727@users.noreply.github.com>
2023-08-22 04:03:25 +0300

    meow

---
+++ main_osu_file.py
@@ -0,0 +1,9 @@
+from players import Players
+
+register_name = input("What is your name?").capitalize()
+register_country = input("Where are you from?").capitalize()
+register_rank = str(input("What rank are you?"))
+
+player1 = Players(register_name, register_country, register_rank)
+
+print(f'Name: {player1.name}\nCountry: {player1.country}\nRank: {player1.rank}')
---
+++ players.py
@@ -0,0 +1,6 @@
+class Players:
+
+    def __init__(self, name, country, rank):
+        self.name = name
+        self.country = country
+        self.rank = rank
